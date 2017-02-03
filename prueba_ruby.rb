0aeaf649c4888b21348366018513b177546e2245
Hugo Duran <hu.durannn@gmail.com>
2017-02-03 12:05:07 -0300

    segundo commit

--- prueba_ruby.rb
+++ prueba_ruby.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 puts ""
 puts "ingresa una opcion"
 puts "1) promedio alumnos"
@@ -10,15 +12,42 @@ option = 0
 
 while option
 
-	opciones = gets.chomp
+	opciones = gets.chomp.to_i
 	case opciones
-		when "1"
+		when 1
 			file = File.open("pruebaru.csv", "r")
-			data=file.read.split
-			file.close
-			suma=0
+			data = file.read.split
+			suma = 0
 			data.each{|e| (suma+=e.to_i)/data.length}
+			file.close
 			puts suma
 			end
-		when "2"
+		end
+		when 2
+			file = File.open("pruebaru.csv", "r")
+			inasistencias = file.readlines
+			puts "inasistencias"
+			inasistencias.each {|e| puts e if e.include?"A"}
+			file.close
+			end
+		end
+		when 3	
+			file = File.open("pruebaru.csv", "r")
+			prom = file.readlines
+			puts "promedio"
+			suma = 0
+			promedio = 0
+			prom.each do |e|
+			suma.to_i += e
+			promedio = suma.to_i/prom.length-1
+			puts "aprobado" if promedio >= 5 
+				else
+					puts "reprobado"
+			end
+		when "4"	
+			exit
+
+		
 			
+
+
